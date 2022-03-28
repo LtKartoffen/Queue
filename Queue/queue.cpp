c72323d8b25f4224ee9bed96461b3292f824faf9
Юрій Краснюк <youra663@kpi.ua>
2022-03-28 20:42:05 +0300

    added methods

---
+++ Queue/queue.cpp
@@ -0,0 +1,69 @@
+// queue.cpp -- Customer's and Queue's methods
+#include "queue.h"
+
+// Queue's methods
+Queue::Queue(int qs) : qsize(qs)
+{
+	front = rear = nullptr;
+	items = 0;
+}
+
+Queue::~Queue()
+{
+	Node* temp;
+	while (front != nullptr)
+	{
+		temp = front;
+		front = front->next;
+		delete temp;
+	}
+}
+
+bool Queue::isempty() const
+{
+	return items == 0;
+}
+
+bool Queue::isfull() const
+{
+	return items == qsize;
+}
+
+// Add Customer in queue
+bool Queue::enqueue(const Item& item)
+{
+	if (isfull())
+		return false;
+	Node* add = new Node;		// creating node
+	add->item = item;
+	add->next = nullptr;
+	items++;
+	if (front == nullptr)
+		front = add;
+	else
+		rear->next = add;
+	rear = add;
+	return true;
+}
+
+// Setting front's element in variable item and then deleting from queue
+bool Queue::dequeue(Item& item)
+{
+	if (front == nullptr)
+		return false;
+	item = front->item;
+	items--;
+	Node* temp = front;
+	front = front->next;
+	delete temp;
+	if (items == 0)
+		rear = nullptr;
+	return true;
+}
+
+// Customer's method
+void Customer::set(long when)
+{
+	processtime = rand() % 3 + 1;
+	arrive = when;
+}
